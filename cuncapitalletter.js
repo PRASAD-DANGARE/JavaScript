5fb78c1cce04bcc906eb0833cd3c645453d1c049
Prasad_Dangare <67819815+PRASAD-DANGARE@users.noreply.github.com>
2020-12-28 18:59:30 +0530

    JavaScript Program To Uncapitalize First Letter

---
+++ cuncapitalletter.js
@@ -0,0 +1,18 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Description     :  JavaScript Program To Uncapitalize The First Letter
+// Function Date   :  28 Dec 2020
+// Function Author :  Prasad Dangare
+// Output          :  prasad Vijaykumar Dangare
+// 
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+function Uncapitalize_first_letter(str)
+{
+    return str.charAt(0).toLowerCase() + str.slice(1);
+  
+}
+   
+console.log(Uncapitalize_first_letter('Prasad Vijaykumar Dangare'));
+   
