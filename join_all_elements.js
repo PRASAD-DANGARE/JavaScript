e476d7c535b976efb961f616cfa67434d3523632
Prasad_Dangare <67819815+PRASAD-DANGARE@users.noreply.github.com>
2021-01-05 18:34:40 +0530

    JavaScript Program To Join All Elements

---
+++ join_all_elements.js
@@ -0,0 +1,16 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Description     :  JavaScript Program To Join All Elements 
+// Function Date   :  05 Jan 2021
+// Function Author :  Prasad Dangare
+// Output          :  BMW GT , MERCEDES CLA , MUSTANG , ENDEAVOUR
+//                 :  BMW GT  +  MERCEDES CLA  +  MUSTANG  +  ENDEAVOUR
+// 
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+
+myCar = ["BMW GT ", " MERCEDES CLA ", " MUSTANG ", " ENDEAVOUR"];
+
+console.log(myCar.join());
+console.log(myCar.join(' + '));
